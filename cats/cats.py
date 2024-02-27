ad197348d781a34ff31fdbfed9318d9c4948b371
gws <gengwenshuai1@yeah.net>
2024-02-27 19:56:22 +0800

    done wpm func

--- cats/cats.py
+++ cats/cats.py
@@ -93,6 +93,7 @@ def wpm(typed, elapsed):
     assert elapsed > 0, 'Elapsed time must be positive'
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    return float(len(typed)*12/elapsed)
     # END PROBLEM 4
 
 
