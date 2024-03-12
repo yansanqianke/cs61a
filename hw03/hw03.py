68f58e9773319ef9bbb025a2c078b727574786d3
gws <gengwenshuai1@yeah.net>
2024-03-12 16:16:27 +0800

    done q1

--- hw03/hw03.py
+++ hw03/hw03.py
@@ -44,11 +44,13 @@ def planet(size):
     """Construct a planet of some size."""
     assert size > 0
     "*** YOUR CODE HERE ***"
+    return ['planet',size]
 
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
     "*** YOUR CODE HERE ***"
+    return w[1]
 
 def is_planet(w):
     """Whether w is a planet."""
